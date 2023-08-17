e7b8bee38ee9ff279a17fed319139497e4091e31
suheb <github@lemoncraft.in>
2023-08-18 02:41:24 +0530

    7th commit

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import styles from '../../styles/Home.module.css';
-
+import React from "react";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import Button from "../Button";
+import { motion } from "framer-motion";
 const index = () => {
   return (
     <div className={styles.appHero}>
-        <h1>You Create.<br/>We Elevate.</h1>
-        <p>A creative digital agency focused on growing brands online.</p>
+      <motion.div className={styles.appHeroTop}>
+        <motion.div
+          initial={{ x: -1000 }}
+          animate={{ x: 0 }}
+          transition={{ ease: "easeOut", duration: 0.6 }}
+        >
+          <h1>You Create.</h1>
+        </motion.div>
+        <motion.div
+          initial={{ x: 1000 }}
+          animate={{ x: 0 }}
+          transition={{ ease: "easeOut", duration: 0.6 }}
+        >
+          <h1>We Elevate.</h1>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeIn", duration: 1.6 }}
+        >
+          <p>A creative digital agency focused on growing brands online.</p>
+        </motion.div>
+      </motion.div>
+      <div className={styles.appHeroBottom}>
+        <Link href="about">our Projects</Link>
+        <Button text="Demo"></Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,23 +1,47 @@
 import React,{useState} from 'react';
 import styles from '../../styles/Navbar.module.css';
 import Button from '../Button';
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Link from 'next/link';
-
+import Modal from 'react-modal';
+import {TiCancel} from 'react-icons/ti'
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
 const index = () => {
-    const [toggle, setToggle] = useState(false)
+    const [toggle, setToggle] = useState(false);
+    let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function afterOpenModal() {
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
+    <>
     <div className={styles.appNavbar}>
         <Link href='/' className={styles.logo}><img src="https://i.ibb.co/sQmnKJs/logo.png" alt=""/><div>Lemon Craft</div></Link>
         <div className={styles.appNavbarMenu}>
-            <Button text='Request a Demo'/>
+            <div onClick={openModal}><Button  text='Request a Demo'></Button></div>
             <div onClick={()=>setToggle(true)} className={styles.menu}>ME<br/>NU</div>
             {toggle &&(
-                <motion.div 
+                <motion.div
                 initial={{y:"-10",opacity:0,scale:0.5}}
                 animate={{ y: 0,opacity:1,scale:1 }}
                 className={styles.screenMenu}>
-                    <div onClick={()=>setToggle(false)} className={styles.menuClose}>Close</div>
+                    <div onClick={()=>setToggle(false)} className={styles.menuClose}><TiCancel/></div>
                     <motion.div className={styles.navlinks}>
                         <Link className={styles.navlink} href='about'>Design</Link>
                         <Link className={styles.navlink} href='about'>Branding</Link>
@@ -26,11 +50,28 @@ const index = () => {
                         <Link className={styles.navlink} href='about'>Why Choose Us</Link>
                         <Link className={styles.navlink} href='about'>How we work</Link>
                     </motion.div>
-                    
                 </motion.div>
             )}
         </div>
     </div>
+    <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal">
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+        </form>
+  </Modal>
+  </>
   )
 }
 
