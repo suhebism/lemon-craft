ebe3cf7c7fcd931d818ed990f9c86593dd9e0c13
suheb <github@lemoncraft.in>
2023-08-17 21:37:54 +0530

    5 commit

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -4,7 +4,8 @@ import styles from '../../styles/Home.module.css';
 const index = () => {
   return (
     <div className={styles.appHero}>
-                
+        <h1>You Create.<br/>We Elevate.</h1>
+        <p>A creative digital agency focused on growing brands online.</p>
     </div>
   )
 }
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,21 +1,31 @@
 import React,{useState} from 'react';
 import styles from '../../styles/Navbar.module.css';
 import Button from '../Button';
-import { motion } from "framer-motion"
+import {motion} from "framer-motion";
+import Link from 'next/link';
 
 const index = () => {
     const [toggle, setToggle] = useState(false)
-    
   return (
     <div className={styles.appNavbar}>
-        <div className={styles.logo}><img src="https://i.ibb.co/sQmnKJs/logo.png" alt="" /></div>
+        <Link href='/' className={styles.logo}><img src="https://i.ibb.co/sQmnKJs/logo.png" alt=""/><div>Lemon Craft</div></Link>
         <div className={styles.appNavbarMenu}>
-            <Button text='Request Demo'/>
+            <Button text='Request a Demo'/>
             <div onClick={()=>setToggle(true)} className={styles.menu}>ME<br/>NU</div>
             {toggle &&(
                 <motion.div 
-                animate={{ x: 100 }} transition={{ delay: 1 }} 
+                initial={{y:"-10",opacity:0,scale:0.5}}
+                animate={{ y: 0,opacity:1,scale:1 }}
                 className={styles.screenMenu}>
+                    <div onClick={()=>setToggle(false)} className={styles.menuClose}>Close</div>
+                    <motion.div className={styles.navlinks}>
+                        <Link className={styles.navlink} href='about'>Design</Link>
+                        <Link className={styles.navlink} href='about'>Branding</Link>
+                        <Link className={styles.navlink} href='about'>Development</Link>
+                        <Link className={styles.navlink} href='about'>About Us</Link>
+                        <Link className={styles.navlink} href='about'>Why Choose Us</Link>
+                        <Link className={styles.navlink} href='about'>How we work</Link>
+                    </motion.div>
                     
                 </motion.div>
             )}
