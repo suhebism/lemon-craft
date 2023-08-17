a12b8a30f541327807f7888ccd26a17287dd96b1
suheb <suhebism@gmail.com>
2023-08-17 19:28:08 +0530

    first commit

---
+++ components/Hero/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../styles/Home.module.css';
+
+const index = () => {
+  return (
+    <div className={styles.appHero}>
+                
+    </div>
+  )
+}
+
+export default index
