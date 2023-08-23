2388a2adfabcf22878c93e1240f7ae2294b7cadd
suheb <github@lemoncraft.in>
2023-08-24 01:03:30 +0530

    13th commit

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../../styles/Footer.module.css'
-import Wrapper from '../Wrapper'
+import Wrapper from '../Wrapper';
+import Button from '../Button'
 const index = () => {
   return (
     <>
@@ -10,10 +11,17 @@ const index = () => {
             <h1>Subscribe to our newsletter.</h1>
             <p>Lorem ipsum dolor sit amet lorem.</p>
             <input type="text" name="" id="" placeholder='Your email id' />
+            <div className={styles.footerBtn}>Submit</div>
           </div>
-          <div className={styles.mobilesUpdate}></div>
+          <div className={styles.mobilesUpdate}>
+            <h1>Whatsapp Updates</h1>
+            <input type="text" name="" id="" placeholder='Your number' />
+            <div className={styles.footerBtn}>Submit</div>
+          </div>
         </div>  
-        <div className={styles.footerRight}></div>  
+        <div className={styles.footerRight}>
+          
+        </div>  
       </div>
     </>
   )
