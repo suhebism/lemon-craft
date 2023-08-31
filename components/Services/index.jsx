ba594ce78b19099e04b293523db18d02f428eaf6
suheb <github@lemoncraft.in>
2023-08-31 15:15:13 +0530

    21st

--- components/Services/index.jsx
+++ components/Services/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
-import  { ScrollRotate } from 'react-scroll-rotate';
+// import  { ScrollRotate } from 'react-scroll-rotate';
 const index = () => {
   return (
     <div className={styles.servicesTab}>
--- components/WhyChooseUs/index.jsx
+++ components/WhyChooseUs/index.jsx
@@ -31,6 +31,35 @@ const index = () => {
         <div className={styles.lowerWhychooseUs}>   
             <h1>Why Choose Us?</h1>
             <p>Sharp-eyed analysis, to construct what you eye,  to elevate your brand’s success.</p>
+            <div className={styles.whychooseUsContent}>
+              <div className={styles.left}>
+                <div className={styles.verticalLine}>
+                  <p>Why?</p>
+                  <img src="https://i.ibb.co/BLGT1fs/scroll-Cursor.png" alt="" />
+                </div>
+                <img className={styles.whychooseUsContentImg} src="https://i.ibb.co/Bft7vRd/Lemon-Slices-01.png" alt="" />
+              </div>
+              <div className={styles.right}>
+                <p>Our dynamic three-stage framework is meticulously designed to suit the unique needs and challenges of your brand and facilitate premier digital experience for you.</p>
+                <div className={styles.contentBottom}>
+                  <div className={styles.verticalLine2}></div>
+                  <div className={styles.contentBottomRight}>
+                    <div className={styles.explore}>
+                      <h3>EXPLORE</h3>
+                      <p>We assign a dedicated team of specialists who deliberate and put in place the most judicious strategies to help create your brand’s market credibil1ity and value.</p>
+                    </div>
+                    <div className={styles.build}>
+                      <h3>BUILD</h3>
+                      <p>We assign a dedicated team of specialists who deliberate and put in place the most judicious strategies to help create your brand’s market credibil1ity and value.</p>
+                    </div>
+                    <div className={styles.grow}> 
+                      <h3>GROW</h3>
+                      <p>We assign a dedicated team of specialists who deliberate and put in place the most judicious strategies to help create your brand’s market credibil1ity and value.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
     </div>
   )
